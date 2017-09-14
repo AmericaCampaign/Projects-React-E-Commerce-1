901d5bb08009da01cac2a498e1c2a9d88e9ae1ff
Jay Cheroske <jay@portal.org>
2017-09-13 17:02:29 -0700

    auth in progress

--- server/routes/Users/auth.js
+++ server/routes/Users/auth.js
@@ -13,6 +13,31 @@ module.exports = (app, passport) => {
       data: req.user
     }))
 
+  app.get('/api/login', (req, res, next) => {
+    passport.authenticate('local-login', (err, user, info) => {
+      if (err) {
+        return next(err)
+      }
+
+      if (!user) {
+        return res.status(200).json({
+          message: info.message
+        })
+      }
+
+      req.logIn(user, (err) => {
+        if (err) {
+          return next(err)
+        }
+
+        return res.status(200).json({
+          message: info && info.message,
+          data: user
+        })
+      })
+    })(req, res, next)
+  })
+
   app.get('/api/get_user', (req, res) =>
     res.status(200).json({
       message: req.user ? 'User session exists' : 'User session does not exist',
